1c392a313142a9a5cab3f6af6cb901e7a120a8d7
Louis <louis_daniel@outlook.fr>
2025-10-11 19:21:52 +0200

    feat: api endpoints

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { DumpOptions, dumpSchema } from "@/src/dump";
+
+/**
+ * Public API for pgdump
+ */
+export { DumpOptions, dumpSchema };
+
+/**
+ * For convenience, we also export a default object.
+ */
+export default {
+	dumpSchema,
+};
